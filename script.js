7c5f822adc579bb9b4d21c4d5f0502024232c084
guilbd <guilbd@hotmail.com>
2021-07-14 19:43:58 -0300

    Add files via upload
    Projeto 1 - HTML, CSS e Javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+function changeImage(img, sexta) {
+    sexta = sexta.jfif
+    document.getElementById("img").src="sexta.jfif";
+    
+}
+
+function backImage(img, segunda){
+    segunda = segunda.jfif
+    document.getElementsByTagName("btn").src="segunda.jfif"
+}
